5310f7e50645344c9dfe2243698748f41e135a4e
Yvan Douenel <y.douenel@coopernet.fr>
2016-02-10 09:29:47 +0100

    custom.js up to put focus on button 'next question'.

--- sites/all/themes/learn/js/custom.js
+++ sites/all/themes/learn/js/custom.js
@@ -1,6 +1,6 @@
 jQuery(function($){
   console.log("hello");
   $('.answering-form .form-item input:first').focus();
-  $( "input[value='Next question']" ).focus();
+  $( "button[value='Next question']" ).focus();
  /* $('.answering-form').css({'border':'1px solid red'});*/
 });
